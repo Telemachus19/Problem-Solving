911cebdbcf4344a43b4bd14dbdb0bc0866e75f24
Ahmed Ashraf <aashrafmohamed84@gmail.com>
2026-01-08 08:20:08 -0500

    solved: CF268-D2-A

---
+++ Codeforces/CF268-D2-A/CF268-D2-A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    const int num_colors = 2;
+    int teams;
+    int outfits = 0;
+    cin >> teams;
+    vector<vector<int>> colors(teams, vector<int>(num_colors, 0));
+
+    for (int i = 0; i < teams; i++)
+    {
+        for (int j = 0; j < num_colors; j++)
+        {
+            cin >> colors[i][j];
+        }
+    }
+    for (int i = 0; i < teams; i++)
+    {
+        for (int j = 0; j < teams; j++)
+        {
+            if (j == i)
+                continue;
+            if (colors[i][0] == colors[j][1])
+                outfits++;
+        }
+    }
+    cout << outfits;
+
+    return 0;
+}
